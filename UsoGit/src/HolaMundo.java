fbab36c0805d7bcdb479dee607d079430668e1d9
Jorgeeeee1234 <145966889+Jorgeeeee1234@users.noreply.github.com>
2023-09-27 10:39:10 +0200

    Update HolaMundo.java

--- UsoGit/src/HolaMundo.java
+++ UsoGit/src/HolaMundo.java
@@ -2,6 +2,9 @@ package UsoGit;
 
 public class HolaMundo {
 	public static void main(String [] args) {
-		System.out.println("Hola !");
+		System.out.println("Hola");
+	}
+	public int sumar (int a, int b){
+		return a+b;
 	}
 }
